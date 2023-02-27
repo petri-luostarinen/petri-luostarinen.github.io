ea6b9264c6c4a9d7f7efb8d7469de3dd2d3e0e3d
Petri Luostarinen <petri0809@gmail.com>
2023-02-27 20:07:33 +0200

    Add deployment action

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     output: 'static',
-    build: {
-        format: 'file'
-    }
+    site: 'https://petri-luostarinen.github.io'
 });

df251707539a68fcdc536dab11eabb847b81bb99
Petri Luostarinen <petri0809@gmail.com>
2024-10-12 13:45:55 +0300

    Print friendly content update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-    output: 'static',
-    site: 'https://petri-luostarinen.github.io'
+  output: "static",
+  site: "https://petri-luostarinen.github.io",
 });
